caaf5d1568653c74c58317e65d634b75966dbdb4
yunus <yunuspolat2528@gmail.com>
2023-07-18 13:54:53 +0200

    OPP

---
+++ OPP.py
@@ -0,0 +1,68 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("-------------------------------------")
+
+# #! Defining Person 
+
+# class Person:
+#     company="clarusway"
+#     department="IT"
+
+# person1=Person()
+# person2=Person()
+
+# print(person1.company)
+# #todo class'a attribute atama
+# Person.job="developer"
+# print(person1.job)
+
+# #todo instenca(nesne)ye attribute atama. hangisine atarsam ona ait olur. classtaki gibi hepsini kavramaz.
+# person2.location="Germany"
+# print(person2.location)
+
+
+
+# #!self keyword 
+
+# ##??
+# Person.test(person1) #person1.test() bunu yazinca bastakini algiliyor. 
+
+
+#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("-------------------------------------")
+
+
